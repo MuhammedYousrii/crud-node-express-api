25fcda5339869bd81adbf062d5f236464964ea5c
MuhammeD YuosrY <MuhammeD YuosrY>
2017-04-19 14:50:08 +0200

    enhance db config

--- server/db-config/mongoose.js
+++ server/db-config/mongoose.js
@@ -2,17 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema ;
 
 
-const dbUsername = 'muhammedyousrii';
-const dbPassword = '251365478';
+const dbUsername = "muhammedyousrii";
+const dbPassword = 2251365478;
+const dbURI = `mongodb://${dbUsername}:${dbPassword}@ds161410.mlab.com:61410/crudexpresstodoapi` ;
+
+
 
 mongoose.Promise = global.Promise ;
-if(mongoose.connect(process.env.MONGOURI ||'mongodb://localhost:27017/todoApp'))
-{
-    console.log('connected')
-    console.log(process.env.MONGOURI);
-}
-
-
+mongoose.connect(dbURI || `mongodb://localhost:27017/todoApp`);
 
 
 
